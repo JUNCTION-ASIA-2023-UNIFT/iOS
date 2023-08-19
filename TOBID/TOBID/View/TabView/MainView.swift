e6acda8cc298cf8d78e872800c5786d4c3e9ec5e
kybeen <rei980627@gmail.com>
2023-08-19 17:35:18 +0900

    [Feat] 메인뷰 구성

--- TOBID/TOBID/View/TabView/MainView.swift
+++ TOBID/TOBID/View/TabView/MainView.swift
@@ -5,12 +5,26 @@
 //  Created by 김영빈 on 2023/08/19.
 //
 
+import MapKit
 import SwiftUI
+
+let deviceWidth = UIScreen.main.bounds.width
+let deviceHeight = UIScreen.main.bounds.height
 
 // MARK: - 메인(내 코스) 뷰
 struct MainView: View {
     var body: some View {
-        Text("메인뷰")
+        VStack {
+            VStack {
+                Text("앱로고")
+                Text("코스 추천 카테고리 뷰")
+            }
+            .frame(width: deviceWidth, height: deviceHeight*0.300)
+            .background(.yellow)
+            
+            MapView(coordinate: CLLocationCoordinate2D(latitude: 35.210871, longitude: 129.068713))
+                .padding(.horizontal, 15)
+        }
     }
 }
 
---
+++ TOBID/TOBID/View/UnityView.swift
@@ -0,0 +1,20 @@
+//
+//  UnityView.swift
+//  TOBID
+//
+//  Created by 김영빈 on 2023/08/19.
+//
+
+import SwiftUI
+
+struct UnityView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UnityView_Previews: PreviewProvider {
+    static var previews: some View {
+        UnityView()
+    }
+}
