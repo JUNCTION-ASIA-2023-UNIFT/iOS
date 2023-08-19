95c18ca44a5585ed94e79dd0027472a162eeb0af
kybeen <rei980627@gmail.com>
2023-08-19 13:46:40 +0900

    [Setting] 프로젝트 생성

---
+++ TOBID/TOBID/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TOBID
+//
+//  Created by 김영빈 on 2023/08/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TOBID/TOBID/TOBIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  TOBIDApp.swift
+//  TOBID
+//
+//  Created by 김영빈 on 2023/08/19.
+//
+
+import SwiftUI
+
+@main
+struct TOBIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
